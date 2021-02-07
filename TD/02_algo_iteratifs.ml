d3c7c5a063cff3867f324a77ac4749239b747219
Faholan <62927863+Faholan@users.noreply.github.com>
2021-02-07 22:24:27 +0100

    Petit oubli

--- TD/02_algo_iteratifs.ml
+++ TD/02_algo_iteratifs.ml
@@ -87,8 +87,10 @@ let duree_max tab =
     for i = 1 to (Array.length tab - 1) do
       if tab.(i) >= tab.(i - 1) then
         current_streak := !current_streak + 1
-      else
-        max_streak := max !max_streak !current_streak
+      else (
+        max_streak := max !max_streak !current_streak ;
+        current_streak := 0
+      )
     done ;
     max !current_streak !max_streak
 ;;
